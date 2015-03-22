00ffdbdf9e904267e7098b07bebf293673e973c0
Netbug @ Edison <darikcr@gmail.com>
2015-03-22 12:13:02 +0000

    Test fixed from hangs

--- edison-client/test.py
+++ edison-client/test.py
@@ -1,10 +1,13 @@
 from wiringx86 import GPIOEdison as GPIO
 import time
 gpio = GPIO(debug=False)
+time.sleep(2)
 gpio.setPWMPeriod(3, 20000000)
 gpio.pinMode(3, gpio.PWM)
+time.sleep(1)
 while 1:
+	print "Cycle..."
 	gpio.analogWrite(3, 6)
-	time.sleep(1023.0)
+	time.sleep(1.5)
 	gpio.analogWrite(3, 15)
-	time.sleep(1023.0)
+	time.sleep(1.5)
